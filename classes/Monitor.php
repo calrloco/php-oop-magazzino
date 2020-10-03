e64353a01142795fed4f7f64035dd02c6cd145ad
carlo briganti <505641nl@saeinstitute.edu>
2020-10-03 13:03:04 +0200

    monitor

---
+++ classes/Monitor.php
@@ -0,0 +1,73 @@
+<?php
+require_once 'Periferiche.php';
+class Monitor extends Periferiche
+{
+    private int $pollici;
+    private string $apectRatio;
+    private string $risoluzione;
+    private string $refreshRate;
+
+    function __construct(
+        $pollici,
+        $apectRatio,
+        $risoluzione,
+        $refreshRate,
+        $quantita,
+        $categoria,
+        $fornitore,
+        $descrizone,
+        $prezzoAquisto,
+        $prezzoVendita
+    ) {
+        $this->pollici = $pollici;
+        $this->apectRatio = $apectRatio;
+        $this->risoluzione = $risoluzione;
+        $this->refreshRate = $refreshRate;
+        parent::__construct(
+            $quantita,
+            $categoria,
+            $fornitore,
+            $descrizone,
+            $prezzoAquisto,
+            $prezzoVendita
+        );
+    }
+    ////getters /////////////////////////
+    public function getPollici()
+    {
+        return $this->pollici;
+    }
+    public function getAspectRatio()
+    {
+        return $this->aspectRatio;
+    }
+    public function getRisoluzione()
+    {
+        return $this->risoluzione;
+    }
+    public function getRefreshRate()
+    {
+        return $this->refreshRate;
+    }
+    //////setters //////////////////
+    public function setPollici($pollici)
+    {
+        if ($pollici <= 120 && $pollici >= 12) {
+            $this->pollici = $pollici;
+        } else {
+            die('Range non ammesso inserisci una grandezza compresa fra 12 e 120');
+        }
+    }
+    public function setAspectRatio($aspectRatio)
+    {
+        $this->aspectRatio = $aspectRatio;
+    }
+    public function setRisouluzione($risouluzione)
+    {
+        $this->risouluzione = $risouluzione;
+    }
+    public function setRefreshRate($refreshRate)
+    {
+        $this->refreshRate = $refreshRate;
+    }
+}
